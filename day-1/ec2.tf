a29c9d9d28da63a6be421da8df31fac2cdcd2d68
Himanshu <himanshupotpose388@gmail.com>
2026-05-15 09:34:13 +0530

    commit

---
+++ day-1/ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "my_instance"{
+    ami = "ami-05d62b9bc5a6ca605"
+    instance_type = "t3.micro"
+    key_name = "global"
+    vpc_security_group_ids = ["sg-089796f61e66ccb82"]
+    tags = {
+        Name = "my_instance"
+        env = "dev"
+    }
+}
