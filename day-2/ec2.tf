18afd16c9b37edd3c767c10c4e42701f3ab335f2
Himanshu <himanshupotpose388@gmail.com>
2026-05-15 11:57:41 +0530

    commit

---
+++ day-2/ec2.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami = var.image_id
+    instance_type = var.instance_type
+    key_name = var.key_pair
+    vpc_security_group_ids = [var.sg_id, aws_security_group.my_sg.id]
+    tags = {
+        Name = "my_instance"
+        env = "dev"
+    }
+
+
+    user_data = <<EOF
+    #!/bin/bash
+    yum install httpd -y
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+    EOF
+}
+
+resource "aws_security_group" "my_sg" {
+    name = "my_sg"
+    description = "new security group"
+    vpc_id = data.aws_vpc.my_vpc.id
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port =80
+        to_port =80
+        protocol = "tcp"
+    }
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+    }
+
+      ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+
+      egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = -1
+    }
+}
+
+data "aws_vpc" "my_vpc" {
+    default = true
+}
+
+variable "image_id" {
+    default = "ami-05d62b9bc5a6ca605"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_pair" {
+    default = "global"
+}
+
+variable "sg_id" {
+    default = "sg-089796f61e66ccb82"
+}
+
+output "public_ip" {
+    value = aws_instance.my_instance.public_ip
+}
