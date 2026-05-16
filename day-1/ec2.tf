201dd3b2c8b64af284071652cffaaffa628ca7be
Himanshu <himanshupotpose388@gmail.com>
2026-05-16 23:53:56 +0530

    changes

--- day-1/ec2.tf
+++ day-1/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_instance"{
     ami = "ami-07a00cf47dbbc844c"
     instance_type = "t3.micro"
-    key_name = "global"
+    key_name = "npm"
     vpc_security_group_ids = ["sg-082cd8b58e65bbb0a"]
     tags = {
         Name = "my_instance"
