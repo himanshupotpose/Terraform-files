a132aece437f8748ac02de35307f4a66b7b3ecaf
Himanshu <himanshupotpose388@gmail.com>
2026-05-26 15:50:35 +0530

    commit

---
+++ day-3/variables.tf
@@ -0,0 +1,16 @@
+variable "image_id" {
+  default     = "ami-07a00cf47dbbc844c"
+}
+
+variable "instance_type" {
+  default     = "t3.micro"
+}
+variable "project" {
+  default     = "cbz"
+}
+variable "key_name" {
+  default     = "npm"
+}
+variable "env" {
+  default     = "dev"
+}
