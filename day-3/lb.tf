25a3f3a16c1959ef5d3d2e34a17b45f87c426fef
Himanshu <himanshupotpose388@gmail.com>
2026-05-29 21:55:19 +0530

    commit

--- day-3/lb.tf
+++ day-3/lb.tf
@@ -1,13 +1,13 @@
-resource "aws_lb_target_group" "home_target_group" {
-    name = "${var.project}-${var.env}-home-tg"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = data.aws_vpc.default.id
-    health_check {
-        enabled = true
-        path = "/"
-        port = "traffic-port"
-        protocol = "HTTP"       
+resource "aws_lb_target_group" "home_target_group" {          // Creating a target group of AWS load balancer .
+    name = "${var.project}-${var.env}-home-tg"                // Name of the target group .
+    port = 80                                                 // Backend server 80 port  traffic received by load balancer .
+    protocol = "HTTP"                                         // Protocol used by load balancer to route traffic to backend server .
+    vpc_id = data.aws_vpc.default.id                          // VPC ID where target group will be created .
+    health_check {                                            // Health check configuration for target group .
+        enabled = true                                        // Enable health check for target group .
+        path = "/"                                            // ALB/ endpoint will be hit for health check .
+        port = "traffic-port"                                 // It will use the same port that is configured for application traffic.
+        protocol = "HTTP"                                     // Health check will be done through HTTP protocol .
     }
 }
 
@@ -37,10 +37,10 @@ resource "aws_lb_target_group" "laptop_target_group" {
     vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_subnets" "my_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+data "aws_subnets" "my_subnets" {           // ye existing subnets ko AWS se fetch kar raha hai.
+  filter {                                  // filtering condition start.
+    name   = "vpc-id"                       // filter name.
+    values = [data.aws_vpc.default.id]      // filter value, ye VPC ID ke basis pe subnets ko filter karega.
   }
 }
 resource "aws_lb" "application_load_balancer" {
