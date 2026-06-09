2d5ec5d2cbdd1a065acecbf7b9208208231f8bd5
Himanshu <himanshupotpose388@gmail.com>
2026-06-09 22:51:55 +0530

    commit

---
+++ day-6/vpc.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "vpc" {}
+
+resource "aws_subnet" "subnet" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    map_public_ip_on_launch  = true
+    tags = {
+        Name = "my_subnet"
+    }
+}
